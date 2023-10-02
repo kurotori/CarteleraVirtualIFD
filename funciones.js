4fc12baba1e5ba1c20c10fb8fa5940955424bd16
Sebastian de los Ángeles <luissebastianangeles@gmail.com>
2023-10-02 16:03:22 -0300

    Modificaciones y avances varios en lista de noticias: ahora carga las noticias en barra lateral con CSS. Se trabaja en el manejo de fechas

--- funciones.js
+++ funciones.js
@@ -5,6 +5,7 @@ const liListaNoticias = document.getElementById("listaNoticias")
 let datosA
 
 async function cargarLateral() {
+    liListaNoticias.innerHTML=""
     //const datos = await fetch('http://localhost:3000/api/verNoticias/')
     //const noticias = await datos.json()
     fetch('http://localhost:3000/api/verNoticias', {
@@ -20,20 +21,38 @@ async function cargarLateral() {
         listado.forEach(noticia => {
             let liNoticia = document.createElement("li")
             let divTitulo = document.createElement("div")
+            divTitulo.classList.add("noticiaTitulo")
             let divFecha = document.createElement("div")
+            divFecha.classList.add("noticiaFecha")
             let divBorde = document.createElement("div")
+            divBorde.classList.add("noticiaBorde")
             
             let fecha_pubN = new Date(noticia.fecha_pub*1000)
-            let fecha_pub = fecha_pubN.getUTCDate()+"/"+fecha_pubN.getUTCMonth()+"/"+fecha_pubN.getUTCFullYear()
+            let fecha_pub = "Publicado el " + fecha_pubN.getUTCDate()+"/"+fecha_pubN.getUTCMonth()+"/"+fecha_pubN.getUTCFullYear()+"<br>"
+            let fecha_cad=""
+    
 
             let fecha_edN = new Date(noticia.fecha_ed*1000)
             let fecha_ed = fecha_edN.getUTCDate()
 
-            let fecha_cadN = new Date(noticia.fecha_cad*1000)
-            let fecha_cad = fecha_cadN.getUTCDate()
+            if (noticia.fecha_cad!=null) {
+                let fecha_cadN = new Date(noticia.fecha_cad*1000)
+                fecha_cad = "Válido hasta " + fecha_cadN.getUTCDate()+"/"+fecha_cadN.getUTCMonth()+"/"+fecha_cadN.getUTCFullYear()+"<br>"
+            }
+            else{
+                fecha_cad=""
+            }
+            
 
-            pNoticia.innerText = noticia.titulo + " - " + fecha_pub
-            divLateral.appendChild(pNoticia)
+            divTitulo.innerHTML=noticia.titulo
+            divFecha.innerHTML=fecha_pub+fecha_cad
+
+
+            liNoticia.appendChild(divTitulo)
+            liNoticia.appendChild(divFecha)
+            liNoticia.appendChild(divBorde)
+            liListaNoticias.appendChild(liNoticia)
+           
         });
     })
 
